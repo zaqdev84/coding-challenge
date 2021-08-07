91adfa7c43c872113139b5aeaf058cd8837372ac
Zeeshan Aqeel <zeeshan.aqeel@zanui.com.au>
2021-08-08 02:08:48 +1000

    file structure for other questions

---
+++ src/QuestionFour.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodingChallenge;
+
+class QuestionFour 
+{
+	
+	/*static function repeatingLettersExists(string $string): bool 
+	{
+		preg_match_all('/([a-z])(?:.*)(\1)+/i', $string, $matches);
+		$result = array_combine($matches[0], array_map('strlen', $matches[0]));
+		return (count($result) > 0) ? false : true;
+	}*/
+}
---
+++ tests/QuestionThreeTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use CodingChallenge\QuestionThree;
+
+class QuestionThreeTest extends TestCase
+{
+
+    /*private $questionOne;
+    public function setUp()
+    {
+        $this->questionOne = new QuestionOne();
+    }
+    /** @test */
+    /*public function it_returns_bool_output()
+    {
+        $string = "";
+        
+        $this->assertIsBool($this->questionOne->repeatingLettersExists($string));
+
+        $string = " ";
+
+        $this->assertIsBool($this->questionOne->repeatingLettersExists($string));
+
+        $string = "asadas";
+
+        $this->assertIsBool($this->questionOne->repeatingLettersExists($string));
+
+        $string = "asd,asd";
+
+        $this->assertIsBool($this->questionOne->repeatingLettersExists($string));
+
+        $string = "documentry";
+
+        $this->assertIsBool($this->questionOne->repeatingLettersExists($string));
+
+    }
+
+    /** @test */
+    /*public function it_returns_false_on_repeated_letters()
+    {
+
+        $string = "adad";
+        $this->assertFalse($this->questionOne->repeatingLettersExists($string));
+
+    }
+
+    /** @test */
+    /*public function it_returns_true_on_non_repeated_letters()
+    {
+
+        $string = "Cloud";
+        $this->assertTrue($this->questionOne->repeatingLettersExists($string));
+
+    }
+
+    /** @test */
+    /*public function it_returns_true_on_empty_string()
+    {
+        $string = "";
+        var_dump($this->questionOne->repeatingLettersExists($string));
+        $this->assertTrue($this->questionOne->repeatingLettersExists($string));
+    }*/
+
+}
